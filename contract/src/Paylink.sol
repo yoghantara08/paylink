ae48e07ffb60e38fb7538485e8aab876eeb5da89
yoghantara08 <gyoghantara@gmail.com>
2025-02-19 14:51:18 +0800

    paylink contract: create stream

--- contract/src/Paylink.sol
+++ contract/src/Paylink.sol
@@ -1,4 +1,101 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.28;
 
-contract Paylink {}
+import {IERC20} from "openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
+import {SafeERC20} from "openzeppelin-contracts/contracts/token/ERC20/utils/SafeERC20.sol";
+
+contract Paylink {
+    using SafeERC20 for IERC20;
+
+    error InvalidAddress();
+    error InvalidAmount();
+    error InvalidDuration();
+    error InsufficientBalance();
+    error StreamNotFound();
+    error Unauthorized();
+
+    struct Stream {
+        address from;
+        address to;
+        address token;
+        uint256 depositAmount;
+        uint256 amountPerSecond;
+        uint256 startTime;
+        uint256 endTime;
+        uint256 remainingBalance;
+    }
+
+    mapping(uint256 => Stream) public streams;
+    uint256 public streamId;
+
+    event StreamCreated(
+        uint256 indexed streamId,
+        address indexed from,
+        address indexed to,
+        address token,
+        uint256 depositAmount,
+        uint256 startTime,
+        uint256 endTime
+    );
+    event StreamCancelled(
+        uint256 indexed streamId,
+        address indexed from,
+        address indexed to,
+        uint256 remainingBalance
+    );
+    event Withdraw(
+        uint256 indexed streamId,
+        address indexed recipient,
+        uint256 amount
+    );
+
+    modifier onlyValidAddress(address _address) {
+        if (_address == address(0) || _address == address(this)) {
+            revert InvalidAddress();
+        }
+        _;
+    }
+
+    function createStream(
+        address _to,
+        uint256 _depositAmount,
+        address _token,
+        uint256 _duration
+    ) public onlyValidAddress(_to) {
+        if (_to == msg.sender) revert InvalidAddress();
+        if (_depositAmount == 0) revert InvalidAmount();
+        if (_duration == 0) revert InvalidDuration();
+
+        IERC20(_token).safeTransferFrom(
+            msg.sender,
+            address(this),
+            _depositAmount
+        );
+
+        uint256 startTime = block.timestamp;
+        uint256 endTime = startTime + _duration;
+        uint256 ratePerSecond = _depositAmount / _duration;
+
+        streamId++;
+        streams[streamId] = Stream({
+            from: msg.sender,
+            to: _to,
+            token: _token,
+            depositAmount: _depositAmount,
+            amountPerSecond: ratePerSecond,
+            startTime: startTime,
+            endTime: endTime,
+            remainingBalance: _depositAmount
+        });
+
+        emit StreamCreated(
+            streamId,
+            msg.sender,
+            _to,
+            _token,
+            _depositAmount,
+            startTime,
+            endTime
+        );
+    }
+}
